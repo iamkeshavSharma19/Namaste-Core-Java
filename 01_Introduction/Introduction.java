efb4e58d1de8f69e373e65aeb3f4ebcce092c74b
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-08-24 20:52:42 +0530

    Core Java Introduction Less Than Half Completed

---
+++ 01_Introduction/Introduction.java
@@ -0,0 +1,4 @@
+//?Why we need Java ?? ==> Before Java C/C++ languages were there,During 1980/90's C/C++ was very famous because It was fast, simple and low level Language [close to hardware]
+
+//&Why Java ?? ==> There was the problem of portability in the languages like C/C++.
+
